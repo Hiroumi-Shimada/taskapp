22fe614ec6327c5d76dbba857a976f772465a6d9
Hiroumi Shimada <hiroumi0713@gmail.com>
2020-09-22 14:16:16 +0900

    タスク管理アプリを制作した。

---
+++ taskapp/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by Hiroumi Shimada on 2020/09/14.
+//  Copyright © 2020 Hiroumi Shimada. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+
+    // タイトル
+    @objc dynamic var title = ""
+
+    // 内容
+    @objc dynamic var contents = ""
+
+    @objc dynamic var category = ""
+    // 日時
+    @objc dynamic var date = Date()
+
+    // id をプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
